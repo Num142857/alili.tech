57e224136b065a8dfba34c3a6d8771cc84ed411b
Fan <incomparbale9527@foxmail.com>
2018-11-16 13:50:51 +0800

    minifyJS

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,16 @@ const htmlmin = require('gulp-htmlmin');
 
 gulp.task('minify', () => {
     return gulp.src('public/**/*.html')
-      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(htmlmin({ 
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        // collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        // removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        // removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        // removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+       }))
       .pipe(gulp.dest('public'));
   });
 
