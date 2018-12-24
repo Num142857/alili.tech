39f0096f5ec2616180ebe6d8162b85a4809681d7
Fan <incomparbale9527@foxmail.com>
2018-12-24 21:35:52 +0800

    fixed

--- gulpfile.js
+++ gulpfile.js
@@ -103,45 +103,45 @@ gulp.task('generate-service-worker', () => {
         swDest: `./public/sw.js`, // 输出 Service worker 文件
         clientsClaim: true, // Service Worker 被激活后使其立即获得页面控制权
         skipWaiting: true, // 强制等待中的 Service Worker 被激活
-        // runtimeCaching: [
-        //     // 配置路由请求缓存 对应 workbox.routing.registerRoute
-        //     {
-        //         urlPattern: /.*\.js/, // 匹配文件
-        //         handler: 'networkFirst' // 网络优先
-        //     },
-        //     {
-        //         urlPattern: /.*\.css/,
-        //         handler: 'staleWhileRevalidate', // 缓存优先同时后台更新
-        //         options: {
-        //             // 这里可以设置 cacheName 和添加插件
-        //             plugins: [
-        //                 {
-        //                     cacheableResponse: {
-        //                         statuses: [0, 200]
-        //                     }
-        //                 }
-        //             ]
-        //         }
-        //     },
-        //     {
-        //         urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
-        //         handler: 'cacheFirst', // 缓存优先
-        //         options: {
-        //             plugins: [
-        //                 {
-        //                     expiration: {
-        //                         maxAgeSeconds: 24 * 60 * 60, // 最长缓存时间,
-        //                         maxEntries: 50 // 最大缓存图片数量
-        //                     }
-        //                 }
-        //             ]
-        //         }
-        //     },
-        //     {
-        //         urlPattern: /.*\.html/,
-        //         handler: 'networkFirst'
-        //     }
-        // ]
+        runtimeCaching: [
+            // 配置路由请求缓存 对应 workbox.routing.registerRoute
+            {
+                urlPattern: /.*\.js/, // 匹配文件
+                handler: 'networkFirst' // 网络优先
+            },
+            {
+                urlPattern: /.*\.css/,
+                handler: 'staleWhileRevalidate', // 缓存优先同时后台更新
+                options: {
+                    // 这里可以设置 cacheName 和添加插件
+                    plugins: [
+                        {
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+                handler: 'cacheFirst', // 缓存优先
+                options: {
+                    plugins: [
+                        {
+                            expiration: {
+                                // maxAgeSeconds: 360, // 最长缓存时间,
+                                maxEntries: 50 // 最大缓存图片数量
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                urlPattern: /.*\.html/,
+                handler: 'networkFirst'
+            }
+        ]
     })
     .then(() => {
         console.info('Service worker generation completed.');
